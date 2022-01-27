8fdf8cb4f151c9be99dc2f0395a2e2db85679a66
navkal <navkal@hotmail.com>
2022-01-27 16:00:16 -0500

    No comment

--- populators/mass_save.py
+++ populators/mass_save.py
@@ -35,18 +35,18 @@ if __name__ == '__main__':
 
     # Refine raw tables
 
-    month_columns = 'jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec,'
+    common_columns = 'year,jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec,'
 
     print( '\n=======> Electric Usage' )
-    numeric_columns = month_columns + 'annual_electric_usage_mwh'
+    numeric_columns = common_columns + 'annual_electric_usage_mwh'
     os.system( 'python mass_save_refine.py -i RawElectricUsage -o ElectricUsage -n {0} -d ../db/{1}'.format( numeric_columns, args.output_filename ) )
 
     print( '\n=======> Gas Usage' )
-    numeric_columns = month_columns + 'annual_gas_usage_therms'
+    numeric_columns = common_columns + 'annual_gas_usage_therms'
     os.system( 'python mass_save_refine.py -i RawGasUsage -o GasUsage -n {0} -d ../db/{1}'.format( numeric_columns, args.output_filename ) )
 
     print( '\n=======> Geographic Report' )
-    numeric_columns = 'annual_electric_usage_mwh,annual_electric_savings_mwh,electric_incentives_$,annual_gas_usage_therms,annual_gas_savings_therms,gas_incentives_$'
+    numeric_columns = 'year,annual_electric_usage_mwh,annual_electric_savings_mwh,electric_incentives_$,annual_gas_usage_therms,annual_gas_savings_therms,gas_incentives_$'
     os.system( 'python mass_save_refine.py -i RawGeographicReport -o GeographicReport -r zip_code -n {0} -d ../db/{1}'.format( numeric_columns, args.output_filename ) )
 
     # Create table of towns
--- populators/mass_save_calculate.py
+++ populators/mass_save_calculate.py
@@ -11,6 +11,87 @@ import util
 
 #------------------------------------------------------
 
+def get_usage( df_group, sector ):
+
+    try:
+        # Retrieve residential row
+        row = df_group[ df_group[util.SECTOR] == sector ]
+
+        # Retrieve usage
+        usage_mwh = row[util.ANNUAL_ELECTRIC_USAGE].values[0]
+        usage_therms = row[util.ANNUAL_GAS_USAGE].values[0]
+
+    except:
+        usage_mwh = None
+        usage_therms = None
+
+    return ( usage_mwh, usage_therms )
+
+def report_findings( sector, usage_mwh, usage_therms, proceeds_mwh, proceeds_therms ):
+
+    print( sector )
+    print( '  mwh: used {}, collected ${}'.format( usage_mwh, proceeds_mwh ) )
+    print( '  therms: used {}, collected ${}'.format( usage_therms, proceeds_therms ) )
+
+
+
+def do_town( town_row ):
+
+    town = town_row[util.TOWN_NAME]
+
+    eb_factor = town_row[util.PCT_ENERGY_BURDENED] / 100
+
+    print( '' )
+    print( '=== {} ==='.format( town ) )
+
+    # Get geographic report for this town
+    df_gr_town = df_gr[ df_gr[util.TOWN_NAME] == town ]
+
+    # Group this town's report by year
+    for idx, df_group in df_gr_town.groupby( by=[util.YEAR] ):
+
+        # Get year of group
+        year = df_group.iloc[0][util.YEAR]
+        print( '' )
+        print( '>', year, '<' )
+
+        # Retrieve surcharge values used in this year
+        surcharge_row = df_ees[ df_ees[util.YEAR] == year ]
+
+        #
+        # df_group typically contains 3 rows: Residential sector, Commercial sector, and Total
+        # We look at Residential and Commercial rows
+        #
+
+        # Residential
+        sector = 'Residential & Low-Income'
+        usage_mwh, usage_therms = get_usage( df_group, sector )
+
+        if usage_mwh and usage_therms:
+
+            # Calculate proceeds
+            proceeds_r1 = ( 1 - eb_factor ) * usage_mwh * surcharge_row[util.RESIDENTIAL_DOL_PER_MWH].values[0]
+            proceeds_r2 = eb_factor * usage_mwh * surcharge_row[util.DISCOUNT_DOL_PER_MWH].values[0]
+            proceeds_mwh = int( proceeds_r1 + proceeds_r2 )
+            proceeds_therms = int( usage_therms * surcharge_row[util.RESIDENTIAL_DOL_PER_THERM].values[0] )
+
+            # Report findings
+            report_findings( sector, usage_mwh, usage_therms, proceeds_mwh, proceeds_therms )
+
+        # Commercial
+        sector = 'Commercial & Industrial'
+        usage_mwh, usage_therms = get_usage( df_group, sector )
+
+        if usage_mwh and usage_therms:
+
+            # Calculate proceeds
+            proceeds_mwh = int( usage_mwh * surcharge_row[util.COMMERCIAL_DOL_PER_MWH].values[0] )
+            proceeds_therms = int( usage_therms * surcharge_row[util.COMMERCIAL_DOL_PER_THERM].values[0] )
+
+            # Report findings
+            report_findings( sector, usage_mwh, usage_therms, proceeds_mwh, proceeds_therms )
+
+
 if __name__ == '__main__':
 
     # Retrieve and validate arguments
@@ -26,37 +107,12 @@ if __name__ == '__main__':
     df_towns = pd.read_sql_table( 'Towns', engine, index_col=util.ID )
     df_ees = pd.read_sql_table( 'EnergyEfficiencySurcharge', engine, index_col=util.ID )
 
-    print( df_gr )
-    print( df_towns )
-    print( df_ees )
-
-    exit()
-
     for index, row in df_towns.iterrows():
-        town = row[util.TOWN_NAME]
-        pct_eb = row[util.PCT_ENERGY_BURDENED]
-        if town == 'Lawrence':
-            print( '!!!' )
-            print( town )
-            print( pct_eb )
-            exit()
-
-
-
-
-
-
-
-
-
+        do_town( row )
 
 
 
     exit()
-
-
-
-
 
 
 
