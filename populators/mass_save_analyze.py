ab13609854be4af36864466c2a3b308758658ce9
navkal <navkal@hotmail.com>
2022-02-25 13:48:00 -0500

    No comment

--- populators/mass_save_analyze.py
+++ populators/mass_save_analyze.py
@@ -190,7 +190,6 @@ if __name__ == '__main__':
     df_elec_ees[util.YEAR] = df_elec_ees[util.YEAR].astype(int)
     df_gas_ees = pd.read_sql_table( 'GasEesRates', engine, index_col=util.ID )
     df_gas_ees[util.YEAR] = df_gas_ees[util.YEAR].astype(int)
-    df_gas_ees[util.COMMERCIAL_RATE] = df_gas_ees[util.COMMERCIAL_RATE].astype(float)
 
     # Initialize dataframe of analysis results
     df_analysis = df_gr.copy( deep=True )
