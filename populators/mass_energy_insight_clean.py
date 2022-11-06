f94b0347142e42a21281376dffe1d519bcec4b2f
navkal <navkal@hotmail.com>
2022-11-06 18:15:49 -0500

    No comment

--- populators/mass_energy_insight_clean.py
+++ populators/mass_energy_insight_clean.py
@@ -19,7 +19,7 @@ DROPPED = 'Dropped'
 UPDATED = 'Updated'
 
 # Handle conflicts between rows that represent same account
-def handle_conflicts( df ):
+def clean_conflicts( df ):
 
     # Find rows that bear conflicting information about a specific account
     sr_b_duped = df.duplicated( subset=[util.ACCOUNT_NUMBER, util.COST_OR_USE], keep=False )
@@ -95,16 +95,33 @@ def handle_conflicts( df ):
     return df, df_history
 
 
+def clean_external_suppliers_electric( df_raw_el, df_mei ):
+    df_clean_el = df_raw_el.copy()
+    df_clean_el[util.ACCOUNT_NUMBER] = df_clean_el[util.ACCOUNT_NUMBER].astype(str)
+    df_clean_el = pd.merge( df_clean_el, df_mei, how='left', on=[util.ACCOUNT_NUMBER] )
+    df_clean_el[util.MEI_TRACKING] = df_clean_el[util.MEI_TRACKING].fillna( util.NO )
+    df_clean_el.insert( df_clean_el.columns.get_loc( util.ACCOUNT_NUMBER ) + 1, util.MEI_TRACKING, df_clean_el.pop( util.MEI_TRACKING ) )
+    return df_clean_el
+
+
+def clean_external_suppliers_gas( df_raw_gas, df_mei ):
+    df_clean_gas = df_raw_gas.copy()
+    df_clean_gas[util.ACCOUNT_NUMBER] = df_clean_gas[util.UTILITY_ACCOUNT].str.split( '-', expand=True )[0]
+    df_clean_gas = pd.merge( df_clean_gas, df_mei, how='left', on=[util.ACCOUNT_NUMBER] )
+    df_clean_gas[util.MEI_TRACKING] = df_clean_gas[util.MEI_TRACKING].fillna( util.NO )
+    df_clean_gas.insert( df_clean_gas.columns.get_loc( util.UTILITY_ACCOUNT ) + 1, util.ACCOUNT_NUMBER, df_clean_gas.pop( util.ACCOUNT_NUMBER ) )
+    df_clean_gas.insert( df_clean_gas.columns.get_loc( util.ACCOUNT_NUMBER ) + 1, util.MEI_TRACKING, df_clean_gas.pop( util.MEI_TRACKING ) )
+    return df_clean_gas
+
+
 # Strip leading zeros from numeric values
 def strip_leading_zeros( sr ):
-
     df_result = sr.to_frame( name='original' )
     df_result['numeric'] = sr[sr.str.isdecimal()]
     df_result['not_numeric'] = sr[~sr.str.isdecimal()]
     df_result['stripped'] = df_result['numeric'].str.replace( r'^0+(?!.*\D)', '', regex=True )
     df_result = df_result.fillna( '' )
     df_result['result'] = df_result['stripped'] + df_result['not_numeric']
-
     return df_result['result']
 
 
@@ -123,6 +140,8 @@ if __name__ == '__main__':
     parser.add_argument( '-e', dest='input_table_electric', help='Input table name: external electricity suppliers' )
     parser.add_argument( '-g', dest='input_table_gas', help='Input table name: external gas suppliers' )
     parser.add_argument( '-o', dest='output_table', help='Output table name' )
+    parser.add_argument( '-p', dest='output_table_electric', help='Output table name: external electricity suppliers' )
+    parser.add_argument( '-q', dest='output_table_gas', help='Output table name: external gas suppliers' )
     args = parser.parse_args()
 
     # Open the database
@@ -130,12 +149,15 @@ if __name__ == '__main__':
 
     # Read raw input table from database
     df_raw = pd.read_sql_table( args.input_table, engine, index_col=util.ID )
+    df = df_raw.copy()
+    df[util.ACCOUNT_NUMBER] = strip_leading_zeros( df[util.ACCOUNT_NUMBER] )
 
     # Read optional table of external electricity suppliers from database
     if args.input_table_electric is not None:
-        df_el = pd.read_sql_table( args.input_table_electric, engine, columns=[util.ACCOUNT_NUMBER, util.LOCATION_ADDRESS] )
+        df_raw_el = pd.read_sql_table( args.input_table_electric, engine, index_col=util.ID )
+        df_el = df_raw_el[ [util.ACCOUNT_NUMBER, util.LOCATION_ADDRESS] ].copy()
     else:
-        df_el = pd.DataFrame( columns=[util.ACCOUNT_NUMBER] )
+        df_raw_el = df_el = pd.DataFrame( columns=[util.ACCOUNT_NUMBER, util.LOCATION_ADDRESS] )
 
     df_es = df_el.copy()
 
@@ -143,7 +165,8 @@ if __name__ == '__main__':
     if args.input_table_gas is not None:
 
         # Read the table
-        df_gas = pd.read_sql_table( args.input_table_gas, engine, columns=[util.UTILITY_ACCOUNT, util.STREET, util.CITY, util.STATE, util.ZIP] )
+        df_raw_gas = pd.read_sql_table( args.input_table_gas, engine, index_col=util.ID )
+        df_gas = df_raw_gas[ [util.UTILITY_ACCOUNT, util.STREET, util.CITY, util.STATE, util.ZIP] ].copy()
 
         # Extract account number
         df_gas[util.ACCOUNT_NUMBER] = df_gas[util.UTILITY_ACCOUNT].str.split( '-', expand=True )[0]
@@ -152,18 +175,12 @@ if __name__ == '__main__':
         df_gas[util.LOCATION_ADDRESS] = df_gas[util.STREET] + ' ' + df_gas[util.CITY] + ' ' + df_gas[util.STATE] + ' ' + df_gas[util.ZIP]
         df_gas = df_gas[ [util.ACCOUNT_NUMBER, util.LOCATION_ADDRESS] ]
     else:
-        df_gas = pd.DataFrame( columns=[util.ACCOUNT_NUMBER, util.LOCATION_ADDRESS] )
+        df_raw_gas = df_gas = pd.DataFrame( columns=[util.ACCOUNT_NUMBER, util.LOCATION_ADDRESS] )
 
     # Combine all external suppliers into single dataframe
     df_es = df_es.append( df_gas, ignore_index=True )
     df_es[util.ACCOUNT_NUMBER] = df_es[util.ACCOUNT_NUMBER].astype(str)
     df_es = df_es.drop_duplicates()
-
-    # Create copy of dataframe
-    df = df_raw.copy()
-
-    # Strip leading zeros from numeric account numbers
-    df[util.ACCOUNT_NUMBER] = strip_leading_zeros( df[util.ACCOUNT_NUMBER] )
 
     # Merge optional external supplier flag into dataframe
     if len( df_es ):
@@ -186,7 +203,7 @@ if __name__ == '__main__':
             df[col_name] = df[col_name].fillna( 0 ).astype( int )
 
     # Handle conflicts between rows representing same account
-    df, df_history = handle_conflicts( df )
+    df, df_history = clean_conflicts( df )
 
     # Save result to database
     util.create_table( args.output_table, conn, cur, df=df )
@@ -194,6 +211,20 @@ if __name__ == '__main__':
     # Optionally save change history to database
     if len( df_history ):
         util.create_table( args.output_table + '_ChangeHistory', conn, cur, df=df_history )
+
+    # Optionally clean and save external supplier tables
+
+    if args.output_table_electric is not None or args.output_table_gas is not None:
+        df_mei = df.copy()
+        df_mei = df_mei[ [util.ACCOUNT_NUMBER] ]
+        df_mei[util.MEI_TRACKING] = util.YES
+        df_mei = df_mei.drop_duplicates()
+
+        if args.output_table_electric is not None:
+            util.create_table( args.output_table_electric, conn, cur, df=clean_external_suppliers_electric( df_raw_el, df_mei ) )
+
+        if args.output_table_gas is not None:
+            util.create_table( args.output_table_gas, conn, cur, df=clean_external_suppliers_gas( df_raw_gas, df_mei ) )
 
     # Report elapsed time
     util.report_elapsed_time()
