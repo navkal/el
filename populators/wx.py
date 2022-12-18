3a1c44833f878b0a45fd9bd05e7e4be6a82bac38
navkal <navkal@hotmail.com>
2022-12-17 22:33:57 -0500

    No comment

---
+++ populators/wx.py
@@ -0,0 +1,145 @@
+# Copyright 2023 Energize Lawrence.  All rights reserved.
+
+import argparse
+import os
+import pandas as pd
+
+import sys
+sys.path.append( '../util' )
+import util
+
+
+# Main program
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser( description='Generate Lawrence master database' )
+    parser.add_argument( '-m', dest='master_filename',  help='Output filename - Name of master database file', required=True )
+    parser.add_argument( '-r', dest='research_filename',  help='Output filename - Name of research database file', required=True )
+    args = parser.parse_args()
+
+    # Read weatherization permit data
+    print( '\n=======> Weatherization permit input' )
+    os.system( 'python xl_to_db.py -i ../xl/wx/building_permits_wx.xlsx -p "Work Description,Use of Property" -t RawBuildingPermits_Wx -o {0} -c'.format( args.master_filename ) )
+
+    # Read GLCAC weatherization project data
+    print( '\n=======> GLCAC weatherization project input' )
+    os.system( 'python xl_to_db.py -i ../xl/wx/glcac_projects.xlsx -t RawGlcacProjects -o {0}'.format( args.master_filename ) )
+
+    # Clean weatherization data
+    print( '\n=======> Clean weatherization data' )
+    os.system( 'python wx_clean.py -m {0}'.format( args.master_filename ) )
+
+
+
+    exit()
+
+
+
+
+
+
+
+
+    # Generate Census table
+    print( '\n=======> Census table' )
+    os.system( 'python lawrence_census.py -m {0}'.format( args.master_filename ) )
+
+    # Read residential assessment data
+    print( '\n=======> Residential input 1' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/assessment/residential_1.xlsx -t RawResidential_1 -r 1 -o {0}'.format( args.master_filename ) )
+    print( '\n=======> Residential input 2' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/assessment/residential_2.xlsx -t RawResidential_2 -r 1 -o {0}'.format( args.master_filename ) )
+    print( '\n=======> Residential input 3' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/assessment/residential_3.xlsx -t RawResidential_3 -r 1 -o {0}'.format( args.master_filename ) )
+
+    # Generate table of residential assessments
+    print( '\n=======> Residential merge' )
+    os.system( 'python lawrence_residential.py -m {0}'.format( args.master_filename ) )
+
+    # Read commercial assessment data
+    print( '\n=======> Commercial input 1' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/assessment/commercial_1.xlsx -t RawCommercial_1 -r 2 -n Location -o {0}'.format( args.master_filename ) )
+    print( '\n=======> Commercial input 2' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/assessment/commercial_2.xlsx -t RawCommercial_2 -r 2 -k "REM_ACCT_NUM,REM_USE_CODE,CNS_OCC,CNS_OCC_DESC" -n REM_USE_CODE -o {0}'.format( args.master_filename ) )
+
+    # Generate table of commercial assessments
+    print( '\n=======> Commercial merge' )
+    os.system( 'python lawrence_commercial.py -m {0}'.format( args.master_filename ) )
+
+    # Read business registration data
+    print( '\n=======> Businesses input 1' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/businesses/businesses_1.xlsx -s License# -t RawBusinesses_1 -o {0}'.format( args.master_filename ) )
+    print( '\n=======> Businesses input 2' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/businesses/businesses_2.xlsx -s License# -t RawBusinesses_2 -o {0}'.format( args.master_filename ) )
+
+    # Generate expanded Businesses table
+    print( '\n=======> Businesses merge' )
+    os.system( 'python lawrence_businesses.py -m {0}'.format( args.master_filename ) )
+
+    # Read city building permit data
+    print( '\n=======> City Building Permit input' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/building_permits/building_permits.xlsx -n "Permit#" -s "Permit#" -t RawBuildingPermits -o {0}'.format( args.master_filename ) )
+
+    # Generate city Building Permits table
+    print( '\n=======> City Building Permits table' )
+    os.system( 'python lawrence_building_permits.py -m {0}'.format( args.master_filename ) )
+
+    # Read Columbia Gas building permit data
+    print( '\n=======> Columbia Gas Building Permit input' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/building_permits/building_permits_columbia_gas.xls -n "Permit #" -u "City/Town,Address Num,Street" -s "Date,Permit #,Address Num,Street" -t RawBuildingPermits_Cga -o {0}'.format( args.master_filename ) )
+
+    # Generate Columbia Gas Building Permits table
+    print( '\n=======> Columbia Gas Building Permits table' )
+    os.system( 'python lawrence_building_permits_cga.py -m {0}'.format( args.master_filename ) )
+
+    # Read solar building permit data
+    print( '\n=======> Solar Building Permit input' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/building_permits/building_permits_solar.xlsx -p "Permit Type,Subtype,Use of Property" -t RawBuildingPermits_Solar -o {0}'.format( args.master_filename ) )
+
+    # Generate Solar Building Permits table
+    print( '\n=======> Solar Building Permits table' )
+    os.system( 'python lawrence_building_permits_solar.py -m {0}'.format( args.master_filename ) )
+
+    # Read Sunrun data
+    print( '\n=======> Sunrun Building Permits input' )
+    os.system( 'python xl_to_db.py -i ../xl/lawrence/building_permits/building_permits_sunrun.xlsx -y -t RawBuildingPermits_Sunrun -o {0}'.format( args.master_filename ) )
+
+    # Generate Sunrun Building Permits table
+    print( '\n=======> Sunrun Building Permits table' )
+    os.system( 'python lawrence_building_permits_sunrun.py -m {0}'.format( args.master_filename ) )
+
+    # Generate copyright notice
+    print( '\n=======> Copyright' )
+    df_about = pd.DataFrame( columns=['copyright'], data=['© 2022 Energize Lawrence.  All rights reserved.'] )
+    util.create_about_table( 'Lawrence', df_about, args.master_filename )
+
+    # Publish research copy of database
+    input_db = util.read_database( args.master_filename )
+    publish_info = \
+    {
+        'number_columns': True,
+        'drop_table_names':
+        [
+            'RawBuildingPermits',
+            'RawBuildingPermits_Cga',
+            'RawBusinesses_1',
+            'RawBusinesses_2',
+            'RawCensus',
+            'RawCommercial_1',
+            'RawCommercial_2',
+            'RawResidential_1',
+            'RawResidential_2',
+            'RawResidential_3',
+            'RawBuildingPermits_Solar',
+            'RawBuildingPermits_Sunrun',
+         ],
+        'encipher_column_names':
+        [
+        ],
+        'drop_column_names':
+        [
+        ]
+    }
+    util.publish_database( input_db, args.research_filename, publish_info )
+
+    util.report_elapsed_time()
---
+++ populators/wx_clean.py
@@ -0,0 +1,60 @@
+# Copyright 2023 Energize Lawrence.  All rights reserved.
+
+import argparse
+
+import pandas as pd
+pd.set_option( 'display.max_columns', 500 )
+pd.set_option( 'display.width', 1000 )
+
+import sys
+sys.path.append( '../util' )
+import util
+import normalize
+
+ADDR = util.NORMALIZED_ADDRESS
+STREET_NUMBER = util.NORMALIZED_STREET_NUMBER
+STREET_NAME = util.NORMALIZED_STREET_NAME
+OCCUPANCY = util.NORMALIZED_OCCUPANCY
+ADDITIONAL = util.NORMALIZED_ADDITIONAL_INFO
+
+
+# Main program
+if __name__ == '__main__':
+
+    # Retrieve and validate arguments
+    parser = argparse.ArgumentParser( description='Generate weatherization records table' )
+    parser.add_argument( '-m', dest='master_filename',  help='Master database filename' )
+    args = parser.parse_args()
+
+    # Open the master database
+    conn, cur, engine = util.open_database( args.master_filename, False )
+
+    # Retrieve city permits table from database
+    df_permits = pd.read_sql_table( 'RawBuildingPermits_Wx', engine, index_col=util.ID, parse_dates=True )
+
+    # Normalize addresses.  Use result_type='expand' to load multiple columns!
+    df_permits[ADDR] = df_permits[util.ADDRESS].str.strip()
+    df_permits[[ADDR,STREET_NUMBER,STREET_NAME,OCCUPANCY,ADDITIONAL]] = df_permits.apply( lambda row: normalize.normalize_address( row, ADDR, city='LAWRENCE', return_parts=True ), axis=1, result_type='expand' )
+
+    # Drop empty columns
+    df_permits = df_permits.dropna( axis='columns', how='all' )
+
+    # Create table in database
+    util.create_table( 'BuildingPermits_L_Wx', conn, cur, df=df_permits )
+
+    # Retrieve GLCAC projects table from database
+    df_projects = pd.read_sql_table( 'RawGlcacProjects', engine, index_col=util.ID, parse_dates=True )
+
+    # Normalize addresses.  Use result_type='expand' to load multiple columns!
+    df_projects[ADDR] = df_projects[util.ADDRESS].str.strip()
+    df_projects[ADDR] = df_projects[ADDR].str.split( ' Ma ', expand=True )[0]
+    df_projects[ADDR] = df_projects[ADDR].str.rsplit( n=1, expand=True )[0]
+    df_projects[[ADDR,STREET_NUMBER,STREET_NAME,OCCUPANCY,ADDITIONAL]] = df_projects.apply( lambda row: normalize.normalize_address( row, ADDR, city='LAWRENCE', return_parts=True ), axis=1, result_type='expand' )
+
+    # Drop empty columns
+    df_projects = df_projects.dropna( axis='columns', how='all' )
+
+    # Create table in database
+    util.create_table( 'GlcacProjects_L', conn, cur, df=df_projects )
+
+    util.report_elapsed_time()
