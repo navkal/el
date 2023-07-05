86f58ca46a4ce8b73982dabd36ca0deab83b3ee6
navkal <navkal@hotmail.com>
2023-07-05 18:36:18 -0400

    No comment

--- populators/lawrence_contractor_activity.py
+++ populators/lawrence_contractor_activity.py
@@ -14,7 +14,7 @@ import util
 COLUMNS = \
 [
     util.YEAR,
-    util.APPLICANT,
+    util.CONTRACTOR_NAME,
     util.PROJECT_TYPE,
     util.PROJECT_COUNT,
     util.TOTAL_PROJECT_COST,
@@ -25,17 +25,18 @@ ROW = dict( ( el, 0 ) for el in COLUMNS )
 
 
 # Analyze contractor activity recorded in specified permit table
-def analyze_contractor_activity( df, table_name, project_type ):
+def analyze_contractor_activity( df, table_name, contractor_column, project_type ):
 
     # Read specified table of building permits
     df_permits = pd.read_sql_table( table_name, engine, index_col=util.ID, parse_dates=True )
     df_permits[util.YEAR] = '20' + df_permits[util.PERMIT_NUMBER].str.split( '-', expand=True )[0].str[-2:]
+    df_permits[util.CONTRACTOR_NAME] = df_permits[contractor_column]
 
     # Analyze per-year activity for each contractor
-    for idx, df_group in df_permits.groupby( by=[util.APPLICANT, util.YEAR] ):
+    for idx, df_group in df_permits.groupby( by=[util.CONTRACTOR_NAME, util.YEAR] ):
 
         ROW[util.YEAR] = df_group.iloc[0][util.YEAR]
-        ROW[util.APPLICANT] = df_group.iloc[0][util.APPLICANT]
+        ROW[util.CONTRACTOR_NAME] = df_group.iloc[0][util.CONTRACTOR_NAME]
         ROW[util.PROJECT_TYPE] = project_type
         ROW[util.PROJECT_COUNT] = len( df_group )
         ROW[util.TOTAL_PROJECT_COST] = df_group[util.PROJECT_COST].sum()
@@ -64,12 +65,12 @@ if __name__ == '__main__':
     df = pd.DataFrame( columns=COLUMNS )
 
     # Analyze contractor activity recorded in specified permit tables
-    df = analyze_contractor_activity( df, 'BuildingPermits_L_Wx', 'wx' )
-    df = analyze_contractor_activity( df, 'BuildingPermits_L_Solar_Summary', 'solar' )
+    df = analyze_contractor_activity( df, 'BuildingPermits_L_Wx', util.BUSINESS_NAME, 'wx' )
+    df = analyze_contractor_activity( df, 'BuildingPermits_L_Solar_Summary', util.APPLICANT, 'solar' )
     df[util.TOTAL_PROJECT_COST] = df[util.TOTAL_PROJECT_COST].round().astype(int)
     df[util.PROJECT_COUNT] = df[util.PROJECT_COUNT].astype(int)
 
     # Save final table of commercial assessments
-    util.create_table( 'BuildingContractorActivity', conn, cur, df=df )
+    util.create_table( 'ContractorActivity_L', conn, cur, df=df )
 
     util.report_elapsed_time()
