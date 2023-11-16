438aa87c5b0667dae6a0fd7e4ff6232292d8c6ac
navkal <navkal@hotmail.com>
2023-11-16 16:30:33 -0500

    No comment

--- tools/compare_lawrence_scrapes.py
+++ tools/compare_lawrence_scrapes.py
@@ -8,9 +8,53 @@ pd.set_option('display.width', 1000)
 
 # Column name mappings
 VISION_ID = 'vision_id'
-MAPPED_VALUE = 'normalized_address'
-OLD = MAPPED_VALUE + '_old'
-NEW = MAPPED_VALUE + '_new'
+
+COLUMNS = ['normalized_address', 'occupancy_households', 'total_occupancy']
+_OLD = '_old'
+_NEW = '_new'
+
+# Compare old and new mappings from Vision ID to specified column
+def compare( df_old, df_new, column ):
+
+    # Isolate columns of interest
+    df_old = df_old[[VISION_ID, column]]
+    df_new = df_new[[VISION_ID, column]]
+
+    # Isolate rows with common Vision IDs
+    df_old = pd.merge( df_old, df_common, how='inner', on=[VISION_ID] )
+    df_new = pd.merge( df_new, df_common, how='inner', on=[VISION_ID] )
+
+    # Select rows with unchanged mappings
+    df_same = pd.merge( df_old, df_new, how='inner', on=[VISION_ID, column] )
+
+    # Find Vision IDs with changed mappings
+    vids_same = set( df_same[VISION_ID] )
+    vids_changed = list( vids_common - vids_same )
+
+    # Report findings
+    print( '' )
+    print( '{}:'.format( column ) )
+    print( 'Number of Vision IDs with unchanged mappings: {}'.format( len( df_same ) ) )
+    print( 'Number of Vision IDs with changed mappings: {}'.format( len( vids_changed ) ) )
+
+    # Load changes into a dataframe
+    df_changed = pd.DataFrame()
+    df_changed[VISION_ID] = vids_changed
+    col_old = column + _OLD
+    col_new = column + _NEW
+
+    if len( vids_changed ):
+        df_changed = pd.merge( df_changed, df_old, how='left' )
+        df_changed = df_changed.rename( columns={ column: col_old } )
+        df_changed = pd.merge( df_changed, df_new, how='left' )
+        df_changed = df_changed.rename( columns={ column: col_new } )
+    else:
+        df_changed[col_old] = None
+        df_changed[col_new] = None
+
+    # Return result
+    return df_changed
+
 
 # Main program
 if __name__ == '__main__':
@@ -18,51 +62,31 @@ if __name__ == '__main__':
     # Compare results of two Vision scrapes:
     # Determine whether any Vision IDs are mapped to changed values
 
-    print( '' )
-    print( 'Looking for changed mappings from "{}" to "{}"'.format( VISION_ID, MAPPED_VALUE ) )
-
     # Read old and new CSV files
     df_old = pd.read_csv( './in/Parcels_L_2023-10-18.csv' )
     df_new = pd.read_csv( '../test/Parcels_L.csv' )
 
-    # Isolate useful columns
-    df_old = df_old[[VISION_ID, MAPPED_VALUE]]
-    df_new = df_new[[VISION_ID, MAPPED_VALUE]]
-
     # Select Vision IDs that are common between the two dataframes
     df_common = pd.merge( df_old, df_new, how='inner', on=[VISION_ID] )
     df_common = df_common[[VISION_ID]]
-
-    # Isolate rows with common Vision IDs
-    df_old = pd.merge( df_old, df_common, how='inner', on=[VISION_ID] )
-    df_new = pd.merge( df_new, df_common, how='inner', on=[VISION_ID] )
-
-    # Select rows with unchanged mappings
-    df_same = pd.merge( df_old, df_new, how='inner', on=[VISION_ID,MAPPED_VALUE] )
-
-    # Find Vision IDs with changed mappings
     vids_common = set( df_common[VISION_ID] )
-    vids_same = set( df_same[VISION_ID] )
-    vids_changed = list( vids_common - vids_same )
 
-    # Report findings
+    # Compare old and new with respect to specified column
+    print( 'Looking for changed mappings from "{}" to {}'.format( VISION_ID, COLUMNS ) )
+    df_result = df_common
+    for column in COLUMNS:
+        df_changed = compare( df_old, df_new, column )
+        df_result = pd.merge( df_result, df_changed, on=[VISION_ID], how='outer' )
+
+    # Isolate rows with changed mappings
+    drop_subset = set( df_result.columns ) - set( [VISION_ID] )
+    df_result = df_result.dropna( subset=drop_subset, how='all' )
+    df_result = df_result.fillna( '' )
+    df_result = df_result.sort_values( by=[VISION_ID] )
+    df_result = df_result.reset_index( drop=True )
+
     print( '' )
-    print( 'Number of common Vision IDs: {}'.format( len( df_common ) ) )
-    print( 'Number of Vision IDs with unchanged mappings: {}'.format( len( df_same ) ) )
-    print( 'Number of Vision IDs with changed mappings: {}'.format( len( vids_changed ) ) )
+    print( 'Result' )
+    print( '' )
+    print( df_result )
 
-    if len( vids_changed ):
-
-        # Construct dataframe showing changed mappings
-        df_changed = pd.DataFrame()
-        df_changed[VISION_ID] = vids_changed
-        df_changed = pd.merge( df_changed, df_old, how='left' )
-        df_changed = df_changed.rename( columns={ MAPPED_VALUE: OLD } )
-        df_changed = pd.merge( df_changed, df_new, how='left' )
-        df_changed = df_changed.rename( columns={ MAPPED_VALUE: NEW } )
-        df_changed = df_changed.sort_values( by=[VISION_ID] )
-        df_changed = df_changed.reset_index( drop=True )
-
-        # Report findings
-        print( '' )
-        print( df_changed )
