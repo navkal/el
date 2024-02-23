9794d66f263f52262e0ccee764d91aef66e17c24
navkal <navkal@hotmail.com>
2024-02-23 17:29:34 -0500

    No comment

--- populators/lawrence_ejscreen.py
+++ populators/lawrence_ejscreen.py
@@ -22,6 +22,7 @@ if __name__ == '__main__':
     # Retrieve arguments
     parser = argparse.ArgumentParser( description='Extract US Census EJSCREEN data pertinent to Lawrence' )
     parser.add_argument( '-i', dest='input_filename',  help='EJSCREEN input csv filename' )
+    parser.add_argument( '-d', dest='doc_filename',  help='EJSCREEN documentation xlsx filename' )
     parser.add_argument( '-o', dest='output_filename',  help='EJSCREEN output database filename' )
     args = parser.parse_args()
 
@@ -38,9 +39,27 @@ if __name__ == '__main__':
     # Sort
     df = df.sort_values( by=[util.CENSUS_GEO_ID] )
 
-    # Save to output database
+    # Save data to output database
     conn, cur, engine = util.open_database( args.output_filename, True )
     util.create_table( 'Ejscreen_L', conn, cur, df=df )
+
+    # Save documentation in database
+    xl_doc = pd.ExcelFile( args.doc_filename )
+    for sheet_name in xl_doc.sheet_names:
+
+        # Read the Excel sheet
+        df_sheet = xl_doc.parse( sheet_name, skiprows=1 )
+
+        # Rename the columns
+        for col_name in df_sheet.columns:
+            fixed_name = '_'.join( col_name.split() ).lower()
+            df_sheet = df_sheet.rename( columns={ col_name: fixed_name } )
+
+        # Rename the table
+        table_name = ''.join( sheet_name.split() )
+
+        # Save the table to the database
+        util.create_table( table_name, conn, cur, df=df_sheet )
 
     # Report elapsed time
     util.report_elapsed_time()
