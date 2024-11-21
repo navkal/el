5e67a9e9a8a425d9d033109a680b8f2c8d06d1f9
navkal <navkal@hotmail.com>
2024-11-21 12:34:07 -0500

    No comment

--- populators/lawrence_wards.py
+++ populators/lawrence_wards.py
@@ -11,7 +11,47 @@ sys.path.append( '../util' )
 import util
 
 
+WARD_PARCELS_COLUMNS = \
+[
+    util.VISION_ID,
+    util.ACCOUNT_NUMBER,
+    util.MBLU,
+    util.OWNER_IS_LOCAL,
+    util.NORMALIZED_STREET_NAME,
+    util.LOCATION,
+    util.YEAR_BUILT,
+    util.AGE,
+    util.BUILDING_COUNT,
+    util.TOTAL_ASSESSED_VALUE,
+    util.HEATING_FUEL_DESC,
+    util.HEATING_TYPE_DESC,
+    util.AC_TYPE_DESC,
+    util.HEAT_AC,
+    util.LAND_USE_CODE,
+    util.LAND_USE_CODE_DESC,
+    util.TOTAL_OCCUPANCY,
+    util.TOTAL_ACRES,
+    util.SALE_PRICE,
+    util.SALE_DATE,
+    util.ZONE,
+    util.OWNER_1_NAME,
+    util.OWNER_2_NAME,
+    util.OWNER_ADDRESS,
+    util.OWNER_ZIP,
+    util.ZIP,
+    util.WARD_NUMBER,
+    util.PRECINCT_NUMBER,
+    util.CENSUS_GEO_ID,
+    util.CENSUS_TRACT,
+    util.CENSUS_BLOCK_GROUP,
+    util.VISION_LINK,
+]
 
+WARD_SUMMARY_COLUMNS = \
+[
+    util.COUNCILOR_NAME,
+    util.WARD_NUMBER,
+]
 
 ##################################
 
@@ -35,52 +75,11 @@ if __name__ == '__main__':
     # Isolate residential properties with known wards
     df_parcels = df_parcels[( df_parcels[util.IS_RESIDENTIAL] == util.YES ) & ( df_parcels[util.WARD_NUMBER].notnull() )]
 
-    # Initialize empty wards summary table
-    summary_columns = \
-    [
-        util.COUNCILOR_NAME,
-        util.WARD_NUMBER,
-    ]
-    df_summary = pd.DataFrame( columns=summary_columns )
-
     # Initialize empty dictionary of ward parcels tables
-    ward_parcels_columns = \
-    [
-        util.VISION_ID,
-        util.ACCOUNT_NUMBER,
-        util.MBLU,
-        util.IS_RESIDENTIAL,
-        util.OWNER_IS_LOCAL,
-        util.NORMALIZED_STREET_NAME,
-        util.LOCATION,
-        util.YEAR_BUILT,
-        util.AGE,
-        util.BUILDING_COUNT,
-        util.TOTAL_ASSESSED_VALUE,
-        util.HEATING_FUEL_DESC,
-        util.HEATING_TYPE_DESC,
-        util.AC_TYPE_DESC,
-        util.HEAT_AC,
-        util.LAND_USE_CODE,
-        util.LAND_USE_CODE_DESC,
-        util.TOTAL_OCCUPANCY,
-        util.TOTAL_ACRES,
-        util.SALE_PRICE,
-        util.SALE_DATE,
-        util.ZONE,
-        util.OWNER_1_NAME,
-        util.OWNER_2_NAME,
-        util.OWNER_ADDRESS,
-        util.OWNER_ZIP,
-        util.ZIP,
-        util.WARD_NUMBER,
-        util.PRECINCT_NUMBER,
-        util.CENSUS_GEO_ID,
-        util.CENSUS_TRACT,
-        util.CENSUS_BLOCK_GROUP,
-        util.VISION_LINK,
-    ]
     dc_ward_parcels_dfs = {}
+
+    # Initialize empty wards summary table
+    df_summary = pd.DataFrame( columns=WARD_SUMMARY_COLUMNS )
 
     # Group parcels by wards
     for idx, df_group in df_parcels.groupby( by=[util.WARD_NUMBER] ):
@@ -90,7 +89,7 @@ if __name__ == '__main__':
         #
 
         # Create parcels table for current ward
-        df_ward_parcels = df_group[ward_parcels_columns]
+        df_ward_parcels = df_group[WARD_PARCELS_COLUMNS]
 
         # Save parcels table for this ward in dictionary
         dc_ward_parcels_dfs[idx] = df_ward_parcels
@@ -112,7 +111,7 @@ if __name__ == '__main__':
 
     # Save ward tables in database
     for ward in dc_ward_parcels_dfs:
-        util.create_table( 'Ward_' + ward, conn, cur, df=dc_ward_parcels_dfs[ward] )
+        util.create_table( 'Ward_{}_ResidentialParcels'.format( ward ), conn, cur, df=dc_ward_parcels_dfs[ward] )
 
     # Save summary table in database
     util.create_table( 'WardSummary', conn, cur, df=df_summary )
