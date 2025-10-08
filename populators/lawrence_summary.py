9cda4555d2dca5c16169c9f14d0caaae6fd5ef5f
navkal <navkal@hotmail.com>
2025-10-07 21:35:20 -0400

    No comment

--- populators/lawrence_summary.py
+++ populators/lawrence_summary.py
@@ -22,6 +22,7 @@ YQ = 'year_quarter'
 PERIOD = 'period'
 WX_COUNT = 'wx_count'
 WX_COUNT_LEAN = 'wx_count_lean'
+
 FUELS = \
 [
     util.ELECTRIC,
@@ -35,31 +36,31 @@ _LEAN = '_lean'
 
 
 
-# Add rows to the per-period weatherization summary
-def add_wx_summary_rows( df_permits, df_summary, period ):
+# Add rows to the weatherization summary
+def add_wx_summary_rows( df_permits, df_summary, attr_name, attr ):
 
-    for idx, df_group in df_permits.groupby( by=[period] ):
+    for idx, df_group in df_permits.groupby( by=[attr] ):
 
-        # Initialize row with period and total weatherization count
-        report_row = {}
-        report_row[PERIOD] = idx
-        report_row[WX_COUNT] = len( df_group )
+        # Initialize row with attribute name and total weatherization count
+        summary_row = {}
+        summary_row[attr_name] = idx
+        summary_row[WX_COUNT] = len( df_group )
 
         # Calculate weatherization subtotals per fuel type
         for s_fuel in FUELS:
-            report_row[s_fuel.lower()] = len( df_group[df_group[util.HEATING_FUEL_DESC] == s_fuel] )
+            summary_row[s_fuel.lower()] = len( df_group[df_group[util.HEATING_FUEL_DESC] == s_fuel] )
 
         # Isolate and count permits at LEAN-eligible parcels
         df_group = df_group[df_group[util.LEAN_ELIGIBILITY] == util.LEAN]
-        report_row[WX_COUNT_LEAN] = len( df_group )
+        summary_row[WX_COUNT_LEAN] = len( df_group )
 
         # Calculate LEAN weatherization subtotals per fuel type
         for s_fuel in FUELS:
             s = s_fuel.lower()
             s_lean = s + _LEAN
-            report_row[s_lean] = len( df_group[df_group[util.HEATING_FUEL_DESC] == s_fuel] )
+            summary_row[s_lean] = len( df_group[df_group[util.HEATING_FUEL_DESC] == s_fuel] )
 
-        df_summary = df_summary.append( report_row, ignore_index=True )
+        df_summary = df_summary.append( summary_row, ignore_index=True )
     return df_summary
 
 
@@ -80,7 +81,7 @@ def make_wx_summary_by_period( conn, cur, engine, output_directory ):
     df_permits[Q] = df_permits[DT].dt.quarter.astype(str)
     df_permits[YQ] = df_permits[Y] + ' Q' + df_permits[Q]
 
-    # Initialize report column order
+    # Initialize summary column order
     summary_columns = [PERIOD, WX_COUNT]
     for s_fuel in FUELS:
         summary_columns.append( s_fuel.lower() )
@@ -88,14 +89,14 @@ def make_wx_summary_by_period( conn, cur, engine, output_directory ):
     for s_fuel in FUELS:
         summary_columns.append( s_fuel.lower() + _LEAN )
 
-    # Initialize empty report dataframe
+    # Initialize empty summary dataframe
     df_summary = pd.DataFrame( columns=summary_columns )
 
     # Add rows per year
-    df_summary = add_wx_summary_rows( df_permits, df_summary, Y )
+    df_summary = add_wx_summary_rows( df_permits, df_summary, PERIOD, Y )
 
     # Add rows per quarter
-    df_summary = add_wx_summary_rows( df_permits, df_summary, YQ )
+    df_summary = add_wx_summary_rows( df_permits, df_summary, PERIOD, YQ )
 
     # Fix numeric types
     for s_fuel in FUELS:
@@ -107,11 +108,56 @@ def make_wx_summary_by_period( conn, cur, engine, output_directory ):
     # Sort on period column
     df_summary = df_summary.sort_values( by=[PERIOD] )
 
-    # Save report to database
+    # Save summary to database
     util.create_table( 'WxSummaryByPeriod', conn, cur, df=df_summary )
 
-    # Save report to excel file
+    # Save summary to excel file
     filename = 'WxSummaryByPeriod.xlsx'
+    filepath = os.path.join( output_directory, filename )
+
+    df_summary.to_excel( filepath, index=False )
+
+
+# Summarize weatherization permit counts by contractor
+def make_wx_summary_by_contractor( conn, cur, engine, output_directory ):
+
+    # Read extended table of weatherization permits from database
+    df_permits = pd.read_sql_table( 'BuildingPermits_L_Wx_Extended', engine, index_col=util.ID, parse_dates=True )
+
+    # Isolate and sort usable rows
+    df_permits = df_permits.dropna( subset=[util.DATE_ISSUED] )
+    df_permits = df_permits[df_permits[util.HEATING_FUEL_DESC].isin( FUELS )]
+    df_permits = df_permits.sort_values( by=[util.DATE_ISSUED] )
+
+    # Initialize summary column order
+    summary_columns = [util.BUSINESS_NAME, WX_COUNT]
+    for s_fuel in FUELS:
+        summary_columns.append( s_fuel.lower() )
+    summary_columns.append( WX_COUNT_LEAN )
+    for s_fuel in FUELS:
+        summary_columns.append( s_fuel.lower() + _LEAN )
+
+    # Initialize empty summary dataframe
+    df_summary = pd.DataFrame( columns=summary_columns )
+
+    # Add rows per contractor
+    df_summary = add_wx_summary_rows( df_permits, df_summary, util.BUSINESS_NAME, util.BUSINESS_NAME )
+
+    # Fix numeric types
+    for s_fuel in FUELS:
+        s = s_fuel.lower()
+        s_lean = s + _LEAN
+        df_summary[s] = df_summary[s].astype( int )
+        df_summary[s_lean] = df_summary[s_lean].astype( int )
+
+    # Sort on contractor column
+    df_summary = df_summary.sort_values( by=[util.BUSINESS_NAME] )
+
+    # Save summary to database
+    util.create_table( 'WxSummaryByContractor', conn, cur, df=df_summary )
+
+    # Save summary to excel file
+    filename = 'WxSummaryByContractor.xlsx'
     filepath = os.path.join( output_directory, filename )
 
     df_summary.to_excel( filepath, index=False )
@@ -123,7 +169,7 @@ def make_wx_summary_by_period( conn, cur, engine, output_directory ):
 if __name__ == '__main__':
 
     # Read arguments
-    parser = argparse.ArgumentParser( description='Generate reports based on contents of master database' )
+    parser = argparse.ArgumentParser( description='Generate summaries based on contents of master database' )
     parser.add_argument( '-m', dest='master_filename',  help='Master database filename', required=True )
     parser.add_argument( '-o', dest='output_directory', help='Target directory output files', required=True )
     parser.add_argument( '-c', dest='clear_directory', action='store_true', help='Clear target directory first?' )
@@ -144,6 +190,8 @@ if __name__ == '__main__':
     # Open the database
     conn, cur, engine = util.open_database( args.master_filename, False )
 
+    # Summarize weatherization permit data
     make_wx_summary_by_period( conn, cur, engine, args.output_directory )
+    make_wx_summary_by_contractor( conn, cur, engine, args.output_directory )
 
     util.report_elapsed_time()
